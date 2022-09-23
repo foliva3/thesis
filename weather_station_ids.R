7f4d5c4ebfb74350e38f8669ed4e73b283984d52
foliva3 <fatimaoliva1217696@gmail.com>
2022-09-22 22:56:53 -0400

    Tricounty stations

---
+++ weather_station_ids.R
@@ -0,0 +1,20 @@
+install.packages('terra')
+library(terra)
+library(rnoaa)
+library(dplyr)
+library(lubridate)
+install.packages('sf')
+library(sf)
+stationsny <- read.csv("c:\\Users\\foliv\\Documents\\thesis data\\station_info.csv")
+counties <- st_read("c:\\Users\\foliv\\Documents\\thesis data\\NYS_Civil_Boundaries.shp\\Counties.shp")
+plot(counties$geometry)
+cny <- counties[counties$NAME =="Oneida"|
+                  counties$NAME =="Madison"|
+                  counties$NAME == "Onondaga",]
+counties.points <- st_as_sf(stationsny, coords = c("long","lat"), crs=4326)
+counties.p <- st_transform(counties.points,st_crs(cny))
+plot(cny$geometry)
+plot(counties.p$geometry, add=TRUE, pch=19)
+stations <- st_intersection(counties.p, cny)
+plot(cny$geometry)
+plot(stations$geometry, add =TRUE)
